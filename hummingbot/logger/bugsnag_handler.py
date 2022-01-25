6a8bf8db0f8f0a361e7c7e1ed67cfa363c48b503
captain13128 <capitan.develop@gmail.com>
2022-01-25 16:57:18 +0300

    [fix] update bugsnag handler

--- hummingbot/logger/bugsnag_handler.py
+++ hummingbot/logger/bugsnag_handler.py
@@ -10,13 +10,12 @@ from hummingbot.client.config.global_config_map import global_config_map
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 
-bugsnag.configure(
-    api_key=global_config_map["bugsnag_api_key"].value,
-    project_root=str(PROJECT_ROOT),
-)
-
-
 class MyBugsnagHandler(BugsnagHandler):
     def __init__(self, client=None, extra_fields=None, level: int = logging.ERROR):
         super(MyBugsnagHandler, self).__init__(client=client, extra_fields=extra_fields)
         self.setLevel(level)
+
+        bugsnag.configure(
+            api_key=global_config_map["bugsnag_api_key"].value,
+            project_root=str(PROJECT_ROOT),
+        )
